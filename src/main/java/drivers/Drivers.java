94fa23d37ccb396f69b2c25a3eeb24200646e8e2
Aykut ABUHAN <abuhanaykut@yahoo.com>
2023-01-12 19:22:17 +0300

    Property Class and File implemented test

--- src/main/java/drivers/Drivers.java
+++ src/main/java/drivers/Drivers.java
@@ -2,25 +2,19 @@ package drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 
 public class Drivers{
 
     protected WebDriver m_driver;
-    private static final String BASE_URL = "https://magento.softwaretestingboard.com/customer/account/create/";
     public void navigateTo(String url){
         m_driver.get(url);
     }
-    @BeforeClass
-    public void setUpDriver(){
+    public void setUpDriver(String baseURL){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         m_driver = new ChromeDriver();
-        navigateTo(BASE_URL);
+        navigateTo(baseURL);
         m_driver.manage().window().maximize();
     }
-
-    @AfterClass
     public void quitDriver(){
         m_driver.quit();
     }
--- src/test/java/test/pages/CreateNewCustomerAccountPageTest.java
+++ src/test/java/test/pages/CreateNewCustomerAccountPageTest.java
@@ -1,13 +1,17 @@
 package test.pages;
 
 import drivers.Drivers;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import page.CreateNewCustomerAccountPage;
+import util.PropertyUtil;
 
 public final class CreateNewCustomerAccountPageTest extends Drivers {
 
     private static CreateNewCustomerAccountPage m_createNewCustomerAccountPage;
+    private static final String baseURL = PropertyUtil.getProperty("CreateAccountURL",
+                                                                                "config.properties");
     private static final String FIRST_NAME = "ExampleName";
     private static final String LAST_NAME = "ExampleLastName";
     private static final String EMAIL = "example1234@gmail.com";
@@ -15,7 +19,12 @@ public final class CreateNewCustomerAccountPageTest extends Drivers {
 
     @BeforeClass
     public void start(){
+        setUpDriver(baseURL);
         m_createNewCustomerAccountPage = new CreateNewCustomerAccountPage(m_driver);
+    }
+    @AfterClass
+    public void finish(){
+        quitDriver();
     }
 
     @Test

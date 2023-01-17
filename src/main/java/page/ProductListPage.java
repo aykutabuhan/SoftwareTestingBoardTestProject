ca81e7f3cbe9d204da5c5f80a460d9bb0d87aa4e
Aykut ABUHAN <abuhanaykut@yahoo.com>
2023-01-18 00:37:22 +0300

    Added assert and changed test methods

--- src/main/java/page/ProductListPage.java
+++ src/main/java/page/ProductListPage.java
@@ -8,57 +8,72 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.List;
 
 public class ProductListPage extends BasePage{
-    @FindBy(css = "div [class='filter-options'] div[class='filter-options-item'] ")
-    private List<WebElement> listOfShoppingOptions;
-    @FindBy(css = "strong[class='product name product-item-name'] a")
+
+    @FindBy(css = "strong[class*='product '] a")
     private List<WebElement> listOfProducts;
-    @FindBy(css = "div[class='swatch-attribute size'] div[class='swatch-attribute-options clearfix'] div")
+    @FindBy(css = "div[class='reviews-actions'] a")
+    private WebElement productClickedMessage;
+    @FindBy(css = "div[aria-label='Size'] div")
     private List<WebElement> listOfProductsSize;
     @FindBy(css = "div[aria-label='Color'] div")
     private List<WebElement> listOfProductColor;
-    @FindBy(css = "div[class='toolbar-sorter sorter'] select[id='sorter']")
+    @FindBy(css = "div[class*='toolbar-sorter'] select[id='sorter']")
     private WebElement sortByButton;
-    @FindBy(css = "div[class='toolbar-sorter sorter'] select[id='sorter'] option")
+    @FindBy(css = "select[id='sorter'] option")
     private List<WebElement> listOfSortBy;
     @FindBy(css = "button[title='Add to Cart']")
     private WebElement addToCartButton;
-    @FindBy(css = "body > div:nth-child(5) > main:nth-child(4) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > div:nth-child(4) > div:nth-child(2) > select:nth-child(1)")
-    private WebElement showProductPerPageButton;
-    @FindBy(css = "div[class='toolbar toolbar-products'] div[class='field limiter'] div[class='control'] select[id='limiter'] option")
+    @FindBy(css = "div[class*='message-success'] div")
+    private WebElement verificationMessage;
+    @FindBy(css = "span[class*='counter']")
+    private WebElement cartCounter;
+    @FindBy(css = "select[id='limiter'] option")
     private List<WebElement> listOfShowPerPage;
+    private static final String HTML_FIELD = "class";
     public ProductListPage(WebDriver driver) {
         super(driver);
     }
-    public void clickShoppingOptionsButtons(){
-        m_webDriverWait.until(ExpectedConditions.visibilityOfAllElements(listOfShoppingOptions));
-        selectAndClickAllOfElements(listOfShoppingOptions);
-    }
+
     public void clickToProduct(int number) {
         m_webDriverWait.until(ExpectedConditions.visibilityOfAllElements(listOfProducts));
         clickElementListWithIndexNo(listOfProducts, number);
-        m_driver.navigate().back();
+    }
+    public String clickedMessageText(){
+        return getText(productClickedMessage);
     }
     public void clickToProductSize(String size){
         m_webDriverWait.until(ExpectedConditions.visibilityOfAllElements(listOfProductsSize));
         m_webDriverWait.until(ExpectedConditions.elementToBeClickable(selectElements(listOfProductsSize, size)))
                                                                                             .click();
     }
+    public boolean isSelectedSize(String HTML_VALUE){
+        return getAttributeText(listOfProductsSize, HTML_FIELD, HTML_VALUE);
+    }
     public void clickToProductsColor(int colorNo){
         m_webDriverWait.until(ExpectedConditions.visibilityOfAllElements(listOfProductColor));
         clickElementListWithIndexNo(listOfProductColor, colorNo);
     }
+    public boolean isSelectedColor(String HTML_VALUE){
+        return getAttributeText(listOfProductColor, HTML_FIELD, HTML_VALUE);
+    }
     public void clickAddToCartButton(){
         centerElement(addToCartButton).click();
+    }
+    public String getVerificationMessage(){
+        waitUntil(ExpectedConditions.visibilityOf(verificationMessage));
+        return getText(verificationMessage);
+    }
+    public String getCartCount(){
+        waitUntil(ExpectedConditions.visibilityOf(cartCounter));
+        return getText(cartCounter);
     }
     public void productListing(String listFilter){
         m_webDriverWait.until(ExpectedConditions.elementToBeClickable(centerElement(sortByButton))).click();
         m_webDriverWait.until(ExpectedConditions.elementToBeClickable(selectElements(listOfSortBy, listFilter)))
                                                                                             .click();
     }
-    public void showNumberOfProductsOnThePage(String number){
-        m_webDriverWait.until(ExpectedConditions.elementToBeClickable(centerElement(showProductPerPageButton)))
-                                                                                            .click();
-        m_webDriverWait.until(ExpectedConditions.elementToBeClickable(selectElements(listOfShowPerPage, number)))
-                                                                                            .click();
+    public String getSortText(String  listFilter){
+        waitUntil(ExpectedConditions.visibilityOf(sortByButton));
+        return getText(listOfSortBy, listFilter);
     }
 }
--- src/test/java/test/pages/ProductListPageTest.java
+++ src/test/java/test/pages/ProductListPageTest.java
@@ -2,26 +2,25 @@ package test.pages;
 
 import drivers.Drivers;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import page.ProductListPage;
-import util.FixedVariateUtil;
 import util.PropertyUtil;
 
 public class ProductListPageTest extends Drivers {
 
     private ProductListPage m_productListPage;
-    private final String NUMBER =  FixedVariateUtil.getFixedVariate("productNumberForShowOnThePage",
-            "fixedVariate.properties");
-    private final int INDEX_NO = Integer.parseInt(FixedVariateUtil.getFixedVariate("productListNumber",
-            "fixedVariate.properties"));
-    private final String PRODUCTS_SIZE = FixedVariateUtil.getFixedVariate("productSize",
-            "fixedVariate.properties");
-    private final int COLOR_NO = Integer.parseInt(FixedVariateUtil.getFixedVariate(
-            "productColorNumber", "fixedVariate.properties"));
-    private final String SORT_PRODUCTS = FixedVariateUtil.getFixedVariate("productSort",
-            "fixedVariate.properties");
+    private static final int PRODUCT_LIST_NUMBER = 0;
+    private static final String PRODUCTS_SIZE = "L";
+    private static final int COLOR_NO = 1;
+    private static final String SORT_PRODUCTS = "Price";
+    private static final String MESSAGE = "You added Oslo Trek Hoodie to your shopping cart.";
+    private static final String CLICKED_MESSAGE = "Be the first to review this product";
+    private static final String HTML_SIZE_MESSAGE = "swatch-option text selected";
+    private static final String HTML_COLOR_MESSAGE = "swatch-option color selected";
+    private static final String CART_COUNTER = "0";
     private static final String baseURL = PropertyUtil.getProperty("HoodieProductListURL",
             "config.properties");
     @BeforeClass
@@ -33,29 +32,26 @@ public class ProductListPageTest extends Drivers {
     public void finish(){
         quitDriver();
     }
+
     @Test
-    public void clickAllOfShoppingOptionsMenu(){
-        m_productListPage.clickShoppingOptionsButtons();
-    }
-    @Test
-    public void showNumberOfProductListOnPage(){
-        m_productListPage.showNumberOfProductsOnThePage(NUMBER);
-    }
-    @Test
-    public void sortByProducts(){
+    public void sortTheProductTest(){
         m_productListPage.productListing(SORT_PRODUCTS);
+        Assert.assertEquals(m_productListPage.getSortText(SORT_PRODUCTS), SORT_PRODUCTS, "Incorrect filter!");
     }
     @Test
-    public void clickToFirstProduct(){
-        m_productListPage.clickToProduct(INDEX_NO);
-    }
-    @Test
-    public void selectSizeAndColor(){
+    public void addToProductInTheCart(){
         m_productListPage.clickToProductSize(PRODUCTS_SIZE);
+        Assert.assertTrue(m_productListPage.isSelectedSize(HTML_SIZE_MESSAGE), "Not selected correct size!");
         m_productListPage.clickToProductsColor(COLOR_NO);
+        Assert.assertTrue(m_productListPage.isSelectedColor(HTML_COLOR_MESSAGE), "Not selected correct color!");
+        m_productListPage.clickAddToCartButton();
+        Assert.assertEquals(m_productListPage.getVerificationMessage(), MESSAGE, "Not the added cart!");
+        Assert.assertNotEquals(m_productListPage.getCartCount(), CART_COUNTER, "Not the added cart!");
     }
     @Test
-    public void clickAddToCart(){
-        m_productListPage.clickAddToCartButton();
+    public void clickFirstProduct(){
+        m_productListPage.clickToProduct(PRODUCT_LIST_NUMBER);
+        Assert.assertEquals(m_productListPage.clickedMessageText(), CLICKED_MESSAGE, "Product is not " +
+                                                                                                    "clickable!");
     }
 }

700908b115ad48358ac26c8df039cf9354b99ceb
Aykut ABUHAN <abuhanaykut@yahoo.com>
2023-01-18 17:55:55 +0300

    Small changes

--- src/main/java/drivers/Drivers.java
+++ src/main/java/drivers/Drivers.java
@@ -2,63 +2,22 @@ package drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import util.URLNavigator;
 
 
-public class Drivers implements ITestListener {
+public class Drivers extends URLNavigator {
     protected WebDriver m_driver;
-    public void navigateTo(String url){
-        m_driver.get(url);
-    }
-    public void setUpDriver(String baseURL){
+
+    @BeforeClass
+    public void setUpDriver(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         m_driver = new ChromeDriver();
-        navigateTo(baseURL);
         m_driver.manage().window().maximize();
     }
+    @AfterClass
     public void quitDriver(){
         m_driver.quit();
-    }
-
-    @Override
-    public void onTestStart(ITestResult result) {
-        ITestListener.super.onTestStart(result);
-    }
-
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        ITestListener.super.onTestSuccess(result);
-    }
-
-    @Override
-    public void onTestFailure(ITestResult result) {
-        ITestListener.super.onTestFailure(result);
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        ITestListener.super.onTestSkipped(result);
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
-    }
-
-    @Override
-    public void onTestFailedWithTimeout(ITestResult result) {
-        ITestListener.super.onTestFailedWithTimeout(result);
-    }
-
-    @Override
-    public void onStart(ITestContext context) {
-        ITestListener.super.onStart(context);
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-        ITestListener.super.onFinish(context);
     }
 }
